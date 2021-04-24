ef74bd14727359d517d9774c31246b4ef8ad72b7
MaybeJaybe <69604238+MaybeJaybe@users.noreply.github.com>
2021-04-23 22:11:39 -0700

    added readme and cleaned up

--- Playlist.py
+++ Playlist.py
@@ -4,17 +4,11 @@ class Playlist:
   def __init__(self):
     self.__first_song = None
 
-
-  # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
-
   def add_song(self, title):
     new_song = Song(title)
     new_song.set_title(title)
     new_song.set_next_song(self.__first_song)
     self.__first_song = new_song
-
-  # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index.
-  # The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index. Otherwise, return -1.
 
   def find_song(self, title):
     current_song = self.__first_song
@@ -30,11 +24,7 @@ class Playlist:
         index += 1
 
     if title_found == False:
-        # put == here if doesnt work
         return -1
-
-
-  # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
 
   def remove_song(self, title):
     current_song = self.__first_song
@@ -51,8 +41,6 @@ class Playlist:
             else:
                 current_song = current_song.get_next_song()
 
-  # TODO: Create a method called length, which returns the number of songs in the playlist.
-
   def length(self):
     index = 0
     current_song = self.__first_song
@@ -63,14 +51,6 @@ class Playlist:
 
     return index
 
-
-  # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
-
-  # Example:
-  # 1. Song Title 1
-  # 2. Song Title 2
-  # 3. Song Title 3
-
   def print_songs(self):
     index = 1
     current_song = self.__first_song
@@ -79,5 +59,3 @@ class Playlist:
         print(f'This is song # {index}, {current_song}')
         index += 1
         current_song = current_song.get_next_song()
-
-        
---
+++ README.md
@@ -0,0 +1,26 @@
+# Welcome to CS 1.2's music playlist assignment. 
+
+the list of TODO's are here:
+
+## In Song.py:
+
+### TODO: Create a getter method for the title attribute, called get_title
+### TODO: Create a setter method for the next_song attribute, called set_title. Make sure titles are type cased to strings and are Title Cased.
+### TODO: Create a getter method for the next_song attribute, called get_next_song
+### TODO: Create a setter method for the next_song attribute, called set_next_song
+### TODO: Using the __str___ dunder method, return a string of the song title.
+### TODO: Using the __repr__ dunder method, return a string formatted as the following:'Song Title -> Next Song Title'
+
+## In Playlist.py:
+
+### TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
+### TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index.
+  # The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index. Otherwise, return -1.
+### TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
+### TODO: Create a method called length, which returns the number of songs in the playlist.
+### TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
+
+  # Example:
+  # 1. Song Title 1
+  # 2. Song Title 2
+  # 3. Song Title 3
